817d92d4de101f74b63c180ca28fd6b08c23ab10
Leobardo Pineda <leobapineda@gmail.com>
2022-06-09 20:44:37 -0500

    Feat: fetch API data with axios and add search recipes with keywords

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useEffect, useState } from "react";
+import axios from "axios";
+import Recipe from "./components/Recipe";
+import './App.css'
 
-function App() {
+export default function App() {
+
+  const YOUR_APP_ID = "1225815e";
+  const YOUR_APP_KEY = "e642963f4d2299d6ac085245011970ab";
+
+  // the initial value of photos has to be something, otherwise the first render will throw and error because it is trying to use map on an undefined element
+  const [recipeName, setRecipeName] = useState(null);
+  const [food, setFood] = useState('')
+  const [submit, setSubmit] = useState('')
+  const [generateKey, setGenerateKey] = useState(1)
+
+  const url2 = "/api/recipes/v2?type=public&q=chicken&app_id=1225815e&app_key=e642963f4d2299d6ac085245011970ab&diet=balanced&random=false"
+
+  const url3 = `https://api.edamam.com/api/recipes/v2?type=public&q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&diet=balanced&random=false`
+
+ useEffect(() => {
+  axios.get(`${url3}`)
+  // .then(res => res.json())
+  .then(APIdata => {
+    setRecipeName(APIdata.data.hits)
+    console.log(APIdata.data)
+  })
+  
+ }, [submit])
+
+ let map = recipeName?.map(item => {
+  //  console.log(item.recipe.mealType);
+  
+   let recipeItem = item.recipe.ingredients.map(element => {
+      return(
+        // <li key={element.text} >{element.text}</li>
+        <li >{element.text}</li>
+      )
+    })
+    
+
+   return(
+      <div key={item.recipe.image} >
+        <div>{item.recipe.label}</div>
+        <img src={item.recipe.image}/>
+        <ul>
+          {recipeItem}
+        </ul>
+      </div>
+   )
+ })
+
+ function handleChange(e) {
+    setFood(e.target.value)
+ }
+
+  function hanbleSubmit(e) {
+    e.preventDefault()
+    setSubmit(food)
+    console.log('submit');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <form onSubmit={hanbleSubmit} >
+      <label id="food" >Choose food</label>
+      <input type='text'
+      name="food input"
+      placeholder="Find a recipe"
+      className="form--input"
+      onChange={handleChange}
+      defaultValue={food}
+
+      />
+<button>Search</button>
+    </form>
+    <div>
+        {map}
     </div>
+    </>
   );
 }
-
-export default App;
