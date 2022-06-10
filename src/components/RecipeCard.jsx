4055719f8af529ebfe885a8b45e1c8b9468ea74f
Leobardo Pineda <leobapineda@gmail.com>
2022-06-10 11:18:55 -0500

    Style: add RecipeCard's text initial style

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -4,16 +4,20 @@ import image from '../images/placeholder-600x600.png'
 import heart from '../images/heart-regular.svg'
 export default function Recipe({img, title, type, origin}) {
     return(
-        <div className="recipe__container" >
+        <article className="recipe__container" >
             <img src={image} alt="recipe img placeholder" />
             <div className="recipe__text-container">
-                <div>dinner</div>
-                <div>Tacos de canasta con todo</div>
-                <div>
-                    <span>Mexican</span>
+                <div className="recipe__meal-type" >dinner</div>
+                <h4 className="recipe__tittle" >Tacos de canasta con todo</h4>
+                <div className="recipe__footer" >
+                    <span className="recipe__cuisine-type" >Mexican</span>
                     <img className="heart" src={heart} alt="heart icon" />
                 </div>
             </div>
-        </div>
+        </article>
     )   
 }
+
+
+
+
