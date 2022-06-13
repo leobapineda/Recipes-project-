f1da05f552dcb80b4ff6065aa59ac862e2ce380e
Leobardo Pineda <leobapineda@gmail.com>
2022-06-13 12:29:35 -0500

    Feat(router): add Link to all navbar elements
    Link is the router element that acts like an "a" tag to move to another page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,23 @@
 import React from "react";
-import '../stylesheet/Navbar.css'
+import "../stylesheet/Navbar.css";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-    return(
-        <nav>
-            
-            <ul className="ul" >
-                <li>
-                    <hr/>
-                    <hr/>
-                    <hr/>
-                </li>
-                <li><a href="/">Site Name</a></li>
-                <li><a className="favorites" href="../Pages/Favorites">Favorites</a></li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav>
+      <ul className="ul">
+        <li>
+          <hr />
+          <hr />
+          <hr />
+        </li>
+        <li>
+          <Link to="/">Linguini</Link>
+        </li>
+        <li>
+          <Link to="favorites">Favorites</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
