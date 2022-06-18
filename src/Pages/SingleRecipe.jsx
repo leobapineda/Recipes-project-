5ec730acd42cce27fbd83e4d07825226d316a194
Leobardo Pineda <leobapineda@gmail.com>
2022-06-18 13:42:55 -0500

    Test(deployed): remove log of TypeError: e is null and add "i am singlerecipe" log

--- src/Pages/SingleRecipe.jsx
+++ src/Pages/SingleRecipe.jsx
@@ -5,7 +5,7 @@ import { useParams, Link, useLocation } from 'react-router-dom'
 function SingleRecipe() {
     
     const { state } = useLocation();
-    
+    console.log("i am singlerecipe");
     const {label, image, source, shareAs} = state
   return (
     <div>
