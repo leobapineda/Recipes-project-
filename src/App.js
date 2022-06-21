6c079ffc093c7f53ab900801667551fdda253658
Leobardo Pineda <leobapineda@gmail.com>
2022-06-21 09:55:23 -0500

    Test(deployed): remove / to favourites and :favorites paths in App and logs 10

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ import SingleRecipe from "./Pages/SingleRecipe";
 import "./App.css";
 
 export default function App() {
-  console.log("i am App 9");
+  console.log("i am App 10");
   return (
     <BrowserRouter>
       <Routes>
         <Route path="linguini-recipes/" element={<SharedLayout />}>
           <Route index element={<Home  />} />
-          <Route path="/:recipeName" element={<SingleRecipe />}  />
-          <Route path="/favorites" element={<Favorites />} />
+          <Route path=":recipeName" element={<SingleRecipe />}  />
+          <Route path="favorites" element={<Favorites />} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
