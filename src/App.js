7ff0569d7325a7ae20223a59d96655c35684d169
Leobardo Pineda <leobapineda@gmail.com>
2022-06-21 10:28:54 -0500

    Test(deployed): add linguini-recipes to all paths in App and logs 14
    Dont add linguini-recipes to the navbar Links

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ import SingleRecipe from "./Pages/SingleRecipe";
 import "./App.css";
 
 export default function App() {
-  console.log("i am App 13");
+  console.log("i am App 14");
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="linguini-recipes" element={<SharedLayout />}>
+        <Route path="linguini-recipes/" element={<SharedLayout />}>
           <Route index element={<Home  />} />
-          <Route path="/:recipeName" element={<SingleRecipe />}  />
-          <Route path="/favorites" element={<Favorites />} />
+          <Route path="linguini-recipes/:recipeName" element={<SingleRecipe />}  />
+          <Route path="linguini-recipes/favorites" element={<Favorites />} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
