211f2e022cb6bc1c403e0b5fbacf2dc336adf26b
Leobardo Pineda <leobapineda@gmail.com>
2022-06-22 10:24:12 -0500

    Test(deployed): replace useLocation for useParams in SingleRecipe-log22

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { React } from "react";
-import { HashRouter ,BrowserRouter, Routes, Route } from "react-router-dom";
-import Error from "./Pages/Error"
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
+import Error from "./Pages/Error";
 import Home from "./Pages/Home";
 import Favorites from "./Pages/Favorites";
 import SharedLayout from "./Pages/SharedLayout";
@@ -8,23 +8,28 @@ import SingleRecipe from "./Pages/SingleRecipe";
 import "./App.css";
 
 export default function App() {
-  console.log("i am App 21");
+  console.log("i am App 22");
   return (
     <HashRouter>
       <Routes>
         <Route path="/linguini-recipes" element={<SharedLayout />}>
-          <Route index element={<Home  />} />
-          <Route path="/linguini-recipes/:recipeName" element={<SingleRecipe />}  />
+          <Route index element={<Home />} />
+          <Route
+            path="/linguini-recipes/:recipeName"
+            element={<SingleRecipe />}
+          />
           <Route path="/linguini-recipes/favorites" element={<Favorites />} />
           <Route path="*" element={<Error />} />
         </Route>
-        {/* <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home  />} />
-          <Route path="/:recipeName" element={<SingleRecipe />}  />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="*" element={<Error />} />
-        </Route> */}
+       
       </Routes>
     </HashRouter>
   );
 }
+
+// {/* <Route path="/" element={<SharedLayout />}>
+//           <Route index element={<Home  />} />
+//           <Route path="/:recipeName" element={<SingleRecipe />}  />
+//           <Route path="/favorites" element={<Favorites />} />
+//           <Route path="*" element={<Error />} />
+//         </Route> */}
