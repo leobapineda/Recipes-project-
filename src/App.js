ac211bf0acad8d00453d268a0e20011f26331889
Leobardo Pineda <leobapineda@gmail.com>
2022-06-22 13:20:04 -0500

    Test(deployed): replace BrowserRouter for HashRouter and remove /linguini in paths in App-log25

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { React } from "react";
-import { HashRouter } from "react-router-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { HashRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Error from "./Pages/Error";
 import Home from "./Pages/Home";
 import Favorites from "./Pages/Favorites";
@@ -9,7 +9,7 @@ import SingleRecipe from "./Pages/SingleRecipe";
 import "./App.css";
 
 export default function App() {
-  console.log("i am App 24");
+  console.log("i am App 25");
   return (
     // <BrowserRouter>
     //   <Routes>
@@ -21,12 +21,22 @@ export default function App() {
     //     </Route>
     //   </Routes>
     // </BrowserRouter>
-    <HashRouter >
+    // <HashRouter>
+    //   <Routes>
+    //     <Route path="/linguini-recipes" element={<SharedLayout />}>
+    //       <Route index element={<Home />} />
+    //       <Route path="/linguini-recipes/:recipeName" element={<SingleRecipe />} />
+    //       <Route path="/linguini-recipes/favorites" element={<Favorites />} />
+    //       <Route path="*" element={<Error />} />
+    //     </Route>
+    //   </Routes>
+    // </HashRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/linguini-recipes" element={<SharedLayout />}>
+        <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
-          <Route path="/linguini-recipes/:recipeName" element={<SingleRecipe />} />
-          <Route path="/linguini-recipes/favorites" element={<Favorites />} />
+          <Route path="/:recipeName" element={<SingleRecipe />} />
+          <Route path="/favorites" element={<Favorites />} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
--- src/Pages/SingleRecipe.jsx
+++ src/Pages/SingleRecipe.jsx
@@ -1,6 +1,6 @@
 import { React } from "react";
 import { Link } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 import { useParams } from "react-router-dom";
 // necesitamos la informaicon de todas las busquedas
 
