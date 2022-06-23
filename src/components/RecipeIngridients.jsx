7639c08067f61bd60a2b4e32af5697b82b3fc17d
Leobardo Pineda <leobapineda@gmail.com>
2022-06-23 17:19:15 -0500

    Feat: add table to RecipeIngridients v1

--- src/components/RecipeIngridients.jsx
+++ src/components/RecipeIngridients.jsx
@@ -1,30 +1,42 @@
 import React from "react";
+import "../stylesheet/RecipeIngridients.css";
 
 function RecipeIngridients({ recipeData }) {
   const { ingredients } = recipeData;
   console.log(ingredients);
   return (
     <div>
-      <h1>What you'll need</h1>
-      <div>
-        <span>Ingredients</span>
-        <span>quantity</span>
-        <span>measure</span>
-      </div>
-      {ingredients.map((element) => {
+      <h1>Ingredients</h1>
+
+      {/* {ingredients.map((element) => {
         return (
           <div>
+
           </div>
         );
-      })}
-      <table>
-        
+      })} */}
+      <table className="table"
+      cellSpacing="0"
+      >
+        <thead className="thead" >
+          <th>Ingredients</th>
+          <th>quantity</th>
+          <th>measure</th>
+        </thead>
+        {ingredients.map((element, index) => {
+          return (
+            <>
+            <tr className="tr"  key={index} >
+              <td className="td1" >{element.food}</td>
+              <td className="td" >{element.quantity}</td>
+              <td className="td" >{element.measure}</td>
+            </tr>
+            </>
+          );
+        })}
       </table>
     </div>
   );
 }
 
 export default RecipeIngridients;
-{/* <span>{element.food}</span>
-            <span>{element.quantity}</span>
-            <span>{element.measure}</span> */}
