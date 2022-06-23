01fc5f991259ef209aacfd95e2b2ec1795dc2abf
Leobardo Pineda <leobapineda@gmail.com>
2022-06-23 16:40:03 -0500

    Feat: add SingleRecipe style

--- src/Pages/SharedLayout.jsx
+++ src/Pages/SharedLayout.jsx
@@ -3,8 +3,6 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 function SharedLayout() {
-  console.log("i am SharedLayout 6");
-
   return (
     <>
       <Navbar />
--- src/Pages/SingleRecipe.jsx
+++ src/Pages/SingleRecipe.jsx
@@ -1,51 +1,42 @@
 import { React } from "react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-// import { useParams } from "react-router-dom";
-// necesitamos la informaicon de todas las busquedas
-import "../stylesheet/SingleRecipe.css"
+import "../stylesheet/SingleRecipe.css";
 function SingleRecipe() {
-  console.log("i am SingleRecipe 20");
-
-  // ------>>>>>> useParams()
-  // const { recipeName } = useParams();
-  // const local = localStorage.getItem("localItem")
-  // const localJson = JSON.parse(local)
-  // const single = localJson?.find((recipe) => {
-  //   return(recipe.recipe.label == recipeName)
-  // })
-
-  // console.log("this is getting the recipe from local storage");
-  // console.log(single);
-  // console.log("this is getting the state from useParams()");
-  // console.log(recipeName);
-
-  // ------>>>>>> useLocation()
   const { state } = useLocation();
   const { label, image, source, shareAs } = state.recipeItem;
-  console.log("i am state");
-  console.log(state);
-  console.log("i am state.recipeItem");
-  console.log(state.recipeItem);
+ console.log(state.recipeItem);
   return (
-    <div>
-      <h1>recipes</h1>
-      <div>{label}</div>
-      <img style={{ width: "250px" }} src={image} />
-      {" "}
-      <div>
-      <a target="_blank" href={shareAs}>
-        View full recipe
-      </a>
+    <>
+    <div className="singleRecipe_main-container">
+      <div className="singleRecipe_text">
+        <p className="singleRecipe_recipes">recipe</p>
+        <h1 className="singleRecipe_tittle">{label}</h1>
+        <div></div>
+        <div>
+          <div className="singleRecipe_source">
+            BY <span> {source}</span>
+          </div>
+        </div>
+        <a className="singleRecipe_source-link singleRecipe_btn " target="_blank" href={shareAs}>
+          Full recipe
+        </a>
+        <h3 className="singleRecipe_servings">
+          {" "}
+          <span>4</span> servings
+        </h3>
+        <Link className="singleRecipe_Link singleRecipe_btn" to="/">
+          Back to home
+        </Link>
       </div>
-      <p>Shared by {source}</p>
-      
-        <Link to= "/" >Back to home</Link> 
-      {/* <Link to="/linguini-recipes">Back to home</Link>
-      {/* ------>>>>>> useParams() */}
-      {/* {`this is the recipe label using useParams():`} 
-      <h1>{recipeName}</h1> */}
+      <div className="singleRecipe_img-container">
+        <img className="singleRecipe_img" 
+        src={image} alt={`${label} img`}/>
+      </div>
     </div>
+    
+    </>
+    
   );
 }
 
